1721c349949a47052de69d850aab001971eb3245
BleepLabs <drbleep@bleeplabs.com>
2013-07-24 22:45:41 -0500

    Update S25FLx.cpp

--- S25FLx.cpp
+++ S25FLx.cpp
@@ -4,6 +4,8 @@ By John-Mike Reed (Dr. Bleep) of Bleep labs
 bleeplabs.com
 
 Usage guide at github.com/BleepLabs/S25FLx/
+Datasheet for S25FL216K www.mouser.com/ds/2/380/S25FL216K_00-6756.pdf
+This library can interface with most of the S25FL family with no modifications. 
 
 This free library is realeased under Creative comoms license CC BY-SA 3.0
 http://creativecommons.org/licenses/by-sa/3.0/deed.en_US
